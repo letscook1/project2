ac42027a026a0d1c614fef74f3e9ff3deb11e264
Vince216 <vinceshury@gmail.com>
2019-05-13 00:23:46 -0400

    added search bar and new input for validating password on creation and have a commented out log-in drop down that could be used instead of an entire page maybe if it functions the same

--- public/js/form.js
+++ public/js/form.js
@@ -1,3 +1,4 @@
+//This file is served up for the account pages, Either create or update account
 const stateDropDownOption = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC',
  'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD',
  'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP',
@@ -5,7 +6,11 @@ const stateDropDownOption = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'D
  'WA', 'WV', 'WI', 'WY' ];
 // jQuery document ready
 $(()=> {
+
     stateDropDownOption.forEach(state => {
         $(".state-options").append("<option>" + state + "</option>");
     });
+
+    //Form submission code can be relocated here. to have it served only on the create/update 
+
 });
