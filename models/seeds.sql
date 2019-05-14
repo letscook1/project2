b729e2b274a0e206696ac1e835490d7b860e017f
mike4747 <mike4747@oh.rr.com>
2019-05-13 21:01:31 -0400

    removed cookie-parser require from server.js, added a few more seeds for cart_items

--- models/seeds.sql
+++ models/seeds.sql
@@ -2,21 +2,21 @@ SET FOREIGN_KEY_CHECKS=0;
 
 -- inserts for products
 INSERT INTO products (name, description, image_name, price, stock, categoryId, createdAt, updatedAt) VALUES
-('$10 bamazon Gift Card', 'Use bamazon Gift Cards to shop at our sister site.', 'bamazon.png', '10', '119', '2', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$25 bamazon Gift Card', 'Use bamazon Gift Cards to shop at our sister site.', 'bamazon.png', '25', '75', '2', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$50 bamazon Gift Card', 'Use bamazon Gift Cards to shop at our sister site.', 'bamazon.png', '50', '155', '2', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$75 bamazon Gift Card', 'Use bamazon Gift Cards to shop at our sister site.', 'bamazon.png', '75', '32', '2', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$100 bamazon Gift Card', 'Use bamazon Gift Cards to shop at our sister site.', 'bamazon.png', '100', '63', '2', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$10 eBook Cash', 'eBook cash can be used at our eBooks sister site.', 'ebooks.png', '10', '76', '1', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$25 eBook Cash', 'eBook cash can be used at our eBooks sister site.', 'ebooks.png', '25', '92', '1', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$50 eBook Cash', 'eBook cash can be used at our eBooks sister site.', 'ebooks.png', '50', '43', '1', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$75 eBook Cash', 'eBook cash can be used at our eBooks sister site.', 'ebooks.png', '75', '16', '1', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$100 eBook Cash', 'eBook cash can be used at our eBooks sister site.', 'ebooks.png', '100', '11', '1', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$10 eGame Cash', 'eGame cash can be used at our eBooks sister site.', 'egames.png', '10', '41', '3', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$25 eGame Cash', 'eGame cash can be used at our eBooks sister site.', 'egames.png', '25', '21', '3', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$50 eGame Cash', 'eGame cash can be used at our eBooks sister site.', 'egames.png', '50', '18', '3', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$75 eGame Cash', 'eGame cash can be used at our eBooks sister site.', 'egames.png', '75', '3', '3', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
-('$100 eGame Cash', 'eGame cash can be used at our eBooks sister site.', 'egames.png', '100', '4', '3', '2019-05-08 21:09:01', '2019-05-08 21:09:01');
+('$10 bamazon Gift Card', 'Use bamazon Gift Cards to shop at our sister site.', 'bamazon.png', '10.00', '119', '2', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$25 bamazon Gift Card', 'Use bamazon Gift Cards to shop at our sister site.', 'bamazon.png', '25.00', '75', '2', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$50 bamazon Gift Card', 'Use bamazon Gift Cards to shop at our sister site.', 'bamazon.png', '50.00', '155', '2', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$75 bamazon Gift Card', 'Use bamazon Gift Cards to shop at our sister site.', 'bamazon.png', '75.00', '32', '2', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$100 bamazon Gift Card', 'Use bamazon Gift Cards to shop at our sister site.', 'bamazon.png', '100.00', '63', '2', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$10 eBook Cash', 'eBook cash can be used at our eBooks sister site.', 'ebooks.png', '10.00', '76', '1', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$25 eBook Cash', 'eBook cash can be used at our eBooks sister site.', 'ebooks.png', '25.00', '92', '1', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$50 eBook Cash', 'eBook cash can be used at our eBooks sister site.', 'ebooks.png', '50.00', '43', '1', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$75 eBook Cash', 'eBook cash can be used at our eBooks sister site.', 'ebooks.png', '75.00', '16', '1', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$100 eBook Cash', 'eBook cash can be used at our eBooks sister site.', 'ebooks.png', '100.00', '11', '1', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$10 eGame Cash', 'eGame cash can be used at our eBooks sister site.', 'egames.png', '10.00', '41', '3', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$25 eGame Cash', 'eGame cash can be used at our eBooks sister site.', 'egames.png', '25.00', '21', '3', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$50 eGame Cash', 'eGame cash can be used at our eBooks sister site.', 'egames.png', '50.00', '18', '3', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$75 eGame Cash', 'eGame cash can be used at our eBooks sister site.', 'egames.png', '75.00', '3', '3', '2019-05-08 21:09:01', '2019-05-08 21:09:01'),
+('$100 eGame Cash', 'eGame cash can be used at our eBooks sister site.', 'egames.png', '100.00', '4', '3', '2019-05-08 21:09:01', '2019-05-08 21:09:01');
 
 -- inserts for categories
 INSERT INTO categories (name, description, image_name) VALUES
@@ -32,8 +32,12 @@ INSERT INTO users (username, password, email, full_name, address, city, state, z
 
 -- inserts for cart_items
 INSERT INTO cart_items (num, each_price, userId, productId) VALUES
-(1, 25.00, 1, 13),
-(1, 50.00, 1, 12),
-(4, 10.00, 1, 6);
+('1', '25.00', '1', '13'),
+('1', '50.00', '1', '12'),
+('4', '10.00', '1', '6'),
+('3', '10.00', '2', '1'),
+('2', '100.00', '2', '5'),
+('1', '75.00', '2', '14'),
+('2', '25.00', '2', '13');
 
 SET FOREIGN_KEY_CHECKS=1;
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 "use strict";
 
 var express = require("express");
-var cookieParser = require('cookie-parser')
 var app = express();
-app.use(cookieParser())
 
 var PORT = process.env.PORT || 3000;
 
